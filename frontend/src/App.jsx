9338fbf76dafc7382a74fde8c1e99b18757df571
SM Ashraf <eshanashraf12@gmail.com>
2025-10-19 00:25:21 -0700

    Almost done with frontend integrations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, use } from 'react'
+import ContactList from './ContactList.jsx'
+import ContactForm from './ContactForm.jsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [contacts, setContacts] = useState([])
+
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  useEffect(() => {
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch('http://localhost:5000/contacts')
+    const data = await response.json()
+    setContacts(data.contacts)
+    console.log(data.contacts)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <> 
+        <ContactList contacts={contacts} />
+        <button onClick={openCreateModal}>Create New Contact</button>
+        { isModalOpen && <div className="modal">
+            <div className="modal-content">
+                <span className="close" onClick={closeModal}>&times;</span>
+                <ContactForm />
+          </div>
+        </div>
+        }
     </>
-  )
+  );
 }
 
 export default App
